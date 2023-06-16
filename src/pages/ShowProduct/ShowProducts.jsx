2c105b77b357c00090fe5f0ffed4d8e4f5699b85
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-06-16 19:22:28 +0400

    connecting to mongoDB

--- src/pages/ShowProduct/ShowProducts.jsx
+++ src/pages/ShowProduct/ShowProducts.jsx
@@ -2,10 +2,6 @@ import React, { useContext, useEffect } from "react";
 import EachProduct from "../../components/AllProduct/AllProduct";
 import Navigation from "../../components/Navigation/Navigation";
 const ShowProducts = () => {
-  useEffect(() => {
-    window.scroll(0, 0);
-    console.log("Hello");
-  }, []);
   return (
     <div>
       <EachProduct />
